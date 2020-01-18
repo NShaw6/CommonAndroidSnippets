16f908be94b6f4c7e57c537af7eee57d811c3b26
Noah Shaw <NShawCS@gmail.com>
2020-01-18 14:37:19 -0500

    Update AccessButtons.java

--- AccessButtons.java
+++ AccessButtons.java
@@ -1,3 +1,10 @@
-    // Access Buttons
+        // Access Buttons
         Button btn = findViewById(R.id.button);
         Log.d("demo", btn.getText().toString());
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("demo", "Button Clicked");
+            }
+        });
